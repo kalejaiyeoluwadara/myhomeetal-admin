09f11720d97b4f69d3582dd0c3ab480e7441e3e8
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-06-17 22:48:49 +0100

    updates

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -8,13 +8,13 @@ export const metadata = {
 
 export default function Layout({ children }) {
   return (
-    <div className="overflow-hidden flex relative ">
+    <div className=" flex overflow-hidden relative ">
       <div className=" relative border-r  w-[272px]">
         <Sidebar />
       </div>
       <div className="flex flex-col w-full items-start justify-start  ">
         <NavBar />
-        <main className="relative w-full ">{children}</main>
+        <main className="relative w-full   ">{children}</main>
         <></>
       </div>
     </div>
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Page() {
-  return <div className="w-full "></div>;
+  return (
+    <div className="w-full bg-red-300 h-[200vh] overflow-y-scroll "> h1 </div>
+  );
 }
 
 export default Page;
