3ac6874ba319c026ed0b8dd11b2aef89f2dead0d
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-06-17 21:02:46 +0100

    form page done

--- app/page.js
+++ app/page.js
@@ -1,9 +1,50 @@
 import Image from "next/image";
-
+import logo from "../app/assets/logo.svg";
+import man from "../app/assets/man.png";
+const Login = () => {
+  return (
+    <div className="border w-[437px] h-[389px] rounded-[24px] flex items-center justify-center p-[24px] flex-col border-[#DCDCDC] gap-[16px] ">
+      <h3 className="text-[25px] font-bold ">Login</h3>
+      <div className="holder">
+        <label className="label">Email Address</label>
+        <input
+          className="inputa"
+          placeholder="Enter Email Address"
+          type="email"
+        />
+      </div>
+      <div className="holder">
+        <label className="label">Password</label>
+        <input
+          className="inputa"
+          placeholder="Enter Password"
+          type="password"
+        />
+      </div>
+      <div className="w-full">
+        <button className=" w-full h-[52px] flex items-center justify-center rounded-[99px] text-[16px] font-bold ">
+          Login
+        </button>
+        <p className="text-[16px] text-center mt-[8px] text-gray-600 font-bold ">
+          Don't have an account{" "}
+          <a href="">
+            <span className="text-primary  ">Create an account</span>
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
 export default function Home() {
   return (
-    <main className="h-screen w-screen bg-white flex items-center justify-center ">
-      <h1>Hello world</h1>
+    <main className="min-h-screen w-full p-[35px] gap-[100px]   flex items-start justify-end ">
+      <section className="h-full flex flex-col items-start  ">
+        <Image className="mb-[81px]" alt="" src={logo} />
+        <Login />
+      </section>
+      <section className=" h-[636px]  overflow-hidden w-[610px] relative rounded-[24px] ">
+        <Image className="cover" src={man} alt="" />
+      </section>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#ED2224",
+        sec: "#0D292A",
+        tertiary: "#354C4D",
+      },
     },
   },
   plugins: [],
