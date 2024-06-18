7cae56c9fa95c98c13a9f93b7ebec99c486609fa
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-06-18 10:00:52 +0100

    updates

---
+++ app/dashboard/SidebarComp/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { FiLogOut } from "react-icons/fi";
+import { TbSettings } from "react-icons/tb";
+import profile from "../../assets/profile.png";
+import { RiCustomerServiceLine } from "react-icons/ri";
+import Image from "next/image";
+function Footer() {
+  return (
+    <footer className="absolute w-full bottom-2  ">
+      <div className="w-full   px-[2px] flex flex-col justify-center items-center ">
+        <div className="w-full flex items-center h-[44px] justify-start px-[16px] gap-[12px]">
+          <TbSettings />
+          <p className="text-[14px] font-normal ">Settings</p>
+        </div>
+        <div className="w-full flex items-center h-[44px] justify-start px-[16px] gap-[12px]">
+          <RiCustomerServiceLine />
+          <p className="text-[14px] font-normal ">Help Center</p>
+        </div>
+      </div>
+      <div className="flex items-center  justify-between py-[12px] px-[16px] gap-2">
+        <div className="flex items-center gap-[12px] justify-center ">
+          <div className="relative flex">
+            <Image
+              src={profile}
+              alt=""
+              className="object-cover h-[40px] w-[40px] "
+            />
+            <div className="absolute right-0 bottom-0 border-2 border-white h-3 w-3 bg-[#04802E] rounded-full " />
+          </div>
+          <div>
+            <h4 className="text-[14px] font-semibold ">David Fayemi</h4>
+            <p className="text-[14px] text-[#475367] ">David@rayna.ui</p>
+          </div>
+        </div>
+        <div>
+          <FiLogOut size={20} />
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- app/dashboard/components/Sidebar.jsx
+++ app/dashboard/components/Sidebar.jsx
@@ -1,22 +1,16 @@
 "use client";
 import Image from "next/image";
 import React, { useState } from "react";
-import { FiLogOut } from "react-icons/fi";
 import logo from "../../assets/logo.svg";
 import finance from "../../assets/finance.svg";
-import chart from "../../assets/orders.svg";
-import cart from "../../assets/cart.svg";
 import home from "../../assets/home.svg";
 import employees from "../../assets/users.svg";
 import { PiUsers } from "react-icons/pi";
-import profile from "../../assets/profile.png";
-import { TbSettings } from "react-icons/tb";
-import { RiCustomerServiceLine } from "react-icons/ri";
-import { IoIosArrowDown } from "react-icons/io";
 import Inner from "../SidebarComp/Inner";
 import EmployeeC from "../SidebarComp/EmployeeC";
 import ProductsC from "../SidebarComp/ProductsC";
 import OrderC from "../SidebarComp/OrderC";
+import Footer from "../SidebarComp/Footer";
 function Sidebar() {
   const [emp, setEmp] = useState(false);
   return (
@@ -55,37 +49,7 @@ function Sidebar() {
       </section>
 
       {/* Footer */}
-      <section className="absolute w-full bottom-2  ">
-        <div className="w-full   px-[2px] flex flex-col justify-center items-center ">
-          <div className="w-full flex items-center h-[44px] justify-start px-[16px] gap-[12px]">
-            <TbSettings />
-            <p className="text-[14px] font-normal ">Settings</p>
-          </div>
-          <div className="w-full flex items-center h-[44px] justify-start px-[16px] gap-[12px]">
-            <RiCustomerServiceLine />
-            <p className="text-[14px] font-normal ">Help Center</p>
-          </div>
-        </div>
-        <div className="flex items-center  justify-between py-[12px] px-[16px] gap-2">
-          <div className="flex items-center gap-[12px] justify-center ">
-            <div>
-              <Image
-                src={profile}
-                alt=""
-                className="object-cover h-[40px] w-[40px] "
-              />
-              <div />
-            </div>
-            <div>
-              <h4 className="text-[14px] font-semibold ">David Fayemi</h4>
-              <p className="text-[14px] text-[#475367] ">David@rayna.ui</p>
-            </div>
-          </div>
-          <div>
-            <FiLogOut size={20} />
-          </div>
-        </div>
-      </section>
+      <Footer />
     </div>
   );
 }
