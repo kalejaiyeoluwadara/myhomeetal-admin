50a7da4702e197fae370ed8b0d755562122aadbd
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-06-19 13:02:28 +0100

    finance

--- app/dashboard/finance/comp/List.jsx
+++ app/dashboard/finance/comp/List.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
 function List() {
-  return <div>List</div>;
+  return (
+    <div className="grid grid-cols-3 mb-6 items-center justify-center gap-4 ">
+      {[
+        {
+          title: "Total Sales",
+          item: "32,540",
+          year: "This year",
+        },
+        {
+          title: "Total Revenue",
+          item: "₦832,540.00",
+          year: "This year",
+        },
+        {
+          title: "Total Withdrawal",
+          item: "₦632,540.00",
+          year: "This year",
+        },
+      ].map((d, id) => {
+        return (
+          <div
+            key={id}
+            className=" h-[150px] border py-4 w-auto rounded-xl bg-white px-4 flex items-start justify-center flex-col "
+          >
+            <section className="flex w-full text-gray-500 justify-between items-center">
+              <p>{d.title}</p>
+              <p>{d.year}</p>
+            </section>
+            <section>
+              <h2 className="text-[32px] font-semibold mt-8 text-[#1D2739] ">
+                {d.item}
+              </h2>
+            </section>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default List;
--- app/dashboard/finance/comp/Transact.jsx
+++ app/dashboard/finance/comp/Transact.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import { BsChevronRight } from "react-icons/bs";
+const Header = () => {
+  return (
+    <header className="flex -top-10 left-0 absolute justify-between items-center w-full">
+      <h2 className="text-[16px] font-semibold text-[#101928] ">
+        Recent Transactions
+      </h2>
+      <p className="text-[#ED2224] font-semibold items-center justify-center flex gap-[6px] text-[14px] ">
+        See all <BsChevronRight size={15} />{" "}
+      </p>
+    </header>
+  );
+};
 function Transact() {
-  return <div>Transact</div>;
+  return (
+    <main>
+      <div className="grid grid-cols-2 mt-20 h-[388px] w-full gap-4 ">
+        <section className="bg-white px-5 py-6 relative w-auto h-full rounded-xl border ">
+          <Header />
+        </section>
+        <section className="bg-[#F0F2F5] w-auto h-full rounded-xl  "></section>
+      </div>
+    </main>
+  );
 }
 
 export default Transact;
