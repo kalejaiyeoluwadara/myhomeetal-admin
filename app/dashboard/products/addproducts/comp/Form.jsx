2b181f10b96df178353719ff728170c9c9c7a489
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-06-20 22:13:50 +0100

    updates

--- app/dashboard/products/addproducts/comp/Form.jsx
+++ app/dashboard/products/addproducts/comp/Form.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { FiUploadCloud } from "react-icons/fi";
-function Form() {
+import { GoChevronDown } from "react-icons/go";
+
+const Modal = () => {
+  const cats = [
+    "My Phones and Tablet",
+    "My Phones and Tablet",
+    "My Phones and Tablet",
+    "My Phones and Tablet",
+    "My Phones and Tablet",
+    "My Phones and Tablet",
+    "My Phones and Tablet",
+  ];
   return (
-    <main className="grid w-full grid-cols-3 mt-10  gap-6 ">
+    <div className="flex flex-col h-[300px] top-24 p-4 right-2 w-[200px] rounded-xl border bg-white absolute z-20 ">
+      {cats.map((d, id) => {
+        return (
+          <p key={id} className="px-2 hover:bg-red-50 py-2">
+            {data}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
+function Form() {
+  const [modal, setModal] = useState(false);
+  return (
+    <main className="grid w-full grid-cols-3 mt-10 mb-40 gap-6 ">
       {/* Main form */}
-      <div className="border bg-white col-span-2 rounded-xl p-4 w-auto h-[937px]">
+      <div className="border bg-white col-span-2 rounded-xl p-4 w-auto h-[800px]">
         {/* title */}
         <h2 className=" core mt-4 ">General Information</h2>
         <section className="mt-4 flex flex-col items-start justify-center gap-6 ">
@@ -45,9 +71,13 @@ function Form() {
               <label className="inputlabel">SKU (Stock Keeping Unit)</label>
               <input className="input" type="text" placeholder="783kl32" />
             </div>
-            <div className="w-full ">
-              <label className="inputlabel">SKU (Stock Keeping Unit)</label>
-              <input className="input" type="text" placeholder="783kl32" />
+            <div className="w-full relative ">
+              <label className="inputlabel">Category</label>
+              <div className="w-full border flex items-center h-[56px] rounded-md  justify-between px-4 ">
+                <p>My Phones and Tablet</p>
+                <GoChevronDown />
+                <Modal />
+              </div>
             </div>
           </section>
         </section>
@@ -55,25 +85,38 @@ function Form() {
 
       {/* Image update */}
       <div className="border bg-white rounded-xl p-4 w-auto h-[536px]">
-        <h2 className="core">Product Image</h2>
-        <p className="text-[]">Set the product media gallery</p>
-        <div>
-          <div className="">
+        <h2 className="core mb-4 ">Product Image</h2>
+        <p className="text-[14px] font-medium text-[#475367] ">
+          Set the product media gallery
+        </p>
+        <div className="p-4 flex w-full border-[1.5px] rounded-md h-[417px] border-dashed border-[#D0D5DD]  items-center justify-center flex-col ">
+          <div className="h-[56px] w-[56px] rounded-full center bg-[#F0F2F5] mb-6 ">
             <FiUploadCloud size={25} />
           </div>
           <div>
-            <h3>
-              <span>Click to upload</span> or drag and drop
+            <h3 className="text-[14px] text-[#475367] ">
+              <span className="text-[14px] text-[#ED2224] ">
+                Click to upload
+              </span>{" "}
+              or drag and drop
             </h3>
-            <p>Max number of file 10 - SVG, PNG, JPG or GIF (max. 800x400px)</p>
+            <p className="text-[12px] text-center text-[#98A2B3]  ">
+              Max number of file 10 - SVG, PNG, JPG or GIF (max. 800x400px)
+            </p>
           </div>
 
-          <div>
-            <hr />
-            <p>OR</p>
+          <div className="my-6 w-full bg-red-300 relative ">
+            <hr className="bg-[#F0F2F5]" />
+            <div className="w-full center ">
+              <p className="absolute px-4 text-center bg-white text-xs font-bold text-[#98A2B3]  -top-[9px] ">
+                OR
+              </p>
+            </div>
           </div>
 
-          <button>Browse Files</button>
+          <button className="w-[118px] center h-[36px] rounded-md text-[14px] font-semibold ">
+            Browse Files
+          </button>
         </div>
       </div>
     </main>
