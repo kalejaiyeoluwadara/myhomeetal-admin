c730399aee5f7fb749744bbe8b662b905a9a5348
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-06-20 07:53:09 +0100

    login updates

--- app/page.js
+++ app/page.js
@@ -4,32 +4,34 @@ import man from "../app/assets/wall.svg";
 import Link from "next/link";
 const Login = () => {
   return (
-    <div className="border w-[437px] h-[389px] rounded-[24px] flex items-center justify-center p-[24px] flex-col border-[#DCDCDC] gap-[16px] ">
-      <h3 className="text-[25px] font-bold ">Login</h3>
-      <div className="holder">
-        <label className="label">Email Address</label>
-        <input
-          className="inputa"
-          placeholder="Enter Email Address"
-          type="email"
-        />
-      </div>
-      <div className="holder">
-        <label className="label">Password</label>
-        <input
-          className="inputa"
-          placeholder="Enter Password"
-          type="password"
-        />
+    <div className="border w-[100%] h-[400px] rounded-[24px] flex items-start justify-center px-[40px] pr-[24px] flex-col border-[#DCDCDC] gap-[16px] ">
+      <h3 className="text-[25px] w-full text-center font-bold ">Login</h3>
+      <div className="flex flex-col w-full gap-2">
+        <div className="holder">
+          <label className="label">Email Address</label>
+          <input
+            className="inputa"
+            placeholder="Enter Email Address"
+            type="email"
+          />
+        </div>
+        <div className="holder mt-4 ">
+          <label className="label">Password</label>
+          <input
+            className="inputa"
+            placeholder="Enter Password"
+            type="password"
+          />
+        </div>
       </div>
       <div className="w-full">
         <Link href="/dashboard">
           {" "}
-          <div className=" w-full h-[52px] bg-primary text-white flex items-center justify-center rounded-[99px] text-[16px] font-bold ">
+          <div className=" mt-4 w-full h-[52px] bg-primary text-white flex items-center justify-center rounded-[99px] text-[16px] font-bold ">
             Login
           </div>
         </Link>
-        <p className="text-[16px] text-center mt-[8px] text-gray-600 font-bold ">
+        <p className="text-[16px] text-center mt-[8px] text-gray-600 font-medium ">
           Don't have an account{" "}
           <a href="">
             <span className="text-primary  ">Create an account</span>
@@ -41,12 +43,12 @@ const Login = () => {
 };
 export default function Home() {
   return (
-    <main className="min-h-screen w-full p-[35px] gap-[100px]   flex items-start justify-end ">
-      <section className="h-full flex flex-col items-start  ">
+    <main className="min-h-screen w-full p-[35px] gap-[60px]   flex items-start justify-center ">
+      <section className="h-full w-[40%] flex flex-col items-start  ">
         <Image className="mb-[81px]" alt="" src={logo} />
         <Login />
       </section>
-      <section className=" h-[636px]  overflow-hidden w-[610px] relative rounded-[24px] ">
+      <section className=" h-[636px]  overflow-hidden w-[50%] relative rounded-[24px] ">
         <Image className="cover" src={man} alt="" />
       </section>
     </main>
