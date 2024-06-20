84302a3ade82e9df636ba4a4fb1b1e5fef959f96
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-06-20 12:39:03 +0100

    updates

--- app/dashboard/categories/[id]/page.jsx
+++ app/dashboard/categories/[id]/page.jsx
@@ -4,21 +4,29 @@ import Image from "next/image";
 import prod from "../../../assets/prod.svg";
 function Page() {
   return (
-    <div className="w-full p-[36px] bg-screen min-h-screen overflow-y-scroll ">
+    <div className="w-full  p-[36px] bg-screen min-h-screen overflow-y-scroll ">
       <Welcome />
       <div className="w-full mt-[34px] ">
-        <div className="border w-full flex px-[] ">
-          <div>
-            <Image src={prod} alt="" className="" />
+        <div className="border relative w-full flex px-6 gap-20 h-[271px] rounded-xl items-center justify-start ">
+          <div className="h-[173px] relative w-[173px] center bg-white rounded-xl ">
+            <Image
+              src={prod}
+              alt=""
+              className=" object-cover h-[131px] w-[131px] "
+            />
           </div>
-          <div>
+          <div className="text[16px] space-y-4 font-light ">
             <p>
               Product Name: Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android 13
             </p>
             <p>Prod Description: ₦145,600</p>
-            <p>SKU</p>
-            <p>SKU</p>
-            <p>Remove Product</p>
+            <p className="flex gap-4">
+              <span>SKU</span>
+              <span>SKU</span>
+            </p>
+            <p className="absolute bottom-4 right-4 cursor-pointer text-[#FF0000] ">
+              Remove Product
+            </p>
           </div>
         </div>
       </div>
