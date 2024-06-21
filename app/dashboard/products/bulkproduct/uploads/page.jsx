8a7cf9fc279576a630566613530089a562a401ca
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-06-21 09:45:32 +0100

    updates

--- app/dashboard/products/bulkproduct/uploads/page.jsx
+++ app/dashboard/products/bulkproduct/uploads/page.jsx
@@ -9,8 +9,8 @@ function Page() {
           Upload Product
         </button>
       </div>
-      <main className="h-[368px] grid grid-cols-3 gap-6 p-6 rounded-[20px] border w-full ">
-        <div className="w-auto border flex-col center rounded-xl h-full ">
+      <main className="h-[368px] flex gap-6 px-6 py-3 rounded-[20px] border w-[900px] ">
+        <div className="w-[235px] border py-3 flex-col center rounded-xl h-full ">
           <div className="h-[50px] w-[50px] rounded-full center bg-[#F0F2F5] mb-6 ">
             <FiUploadCloud size={25} />
           </div>
@@ -21,27 +21,35 @@ function Page() {
             Max number of file 10 - SVG, PNG, JPG or GIF (max. 800x400px)
           </p>
         </div>
-        <div className="col-span-2 flex gap-1 w-auto h-full ">
-          <div className=" flex flex-col gap-4 w-full h-full  ">
-            <p className="text-base font-light">
-              Product Name: Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android 13
-              - Light Green
-            </p>
-            <p className="text-base font-light">
-              Product Name: Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android 13
-              - Light Green
-            </p>
-            <p className="text-base font-light">
-              Product Name: Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android 13
-              - Light Green
-            </p>
-            <p className="text-base font-light">
-              Product Name: Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android 13
-              - Light Green
-            </p>
+        <div className=" flex w-[60%] h-full ">
+          <div className=" flex py-3 flex-col gap-4 w-[80%] h-full  ">
+            <div>
+              <p className="text-base truncate  font-light">
+                Product Name: Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android
+                13 - Light Green
+              </p>
+              <p className="text-base font-light">Prod Description:₦145,600</p>
+              <p className="text-base font-light">SKU SKU</p>
+              <p className="text-base font-light">
+                Product Name: Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android
+                13 - Light Green
+              </p>
+            </div>
+            <div>
+              <p className="text-base  truncate font-light">
+                Product Name: Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android
+                13 - Light Green
+              </p>
+              <p className="text-base font-light">Prod Description:₦145,600</p>
+              <p className="text-base font-light">SKU SKU</p>
+              <p className="text-base font-light">
+                Product Name: Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android
+                13 - Light Green
+              </p>
+            </div>
           </div>
-          <div className="h-full justify-between bg-red-200 items-end flex flex-col ">
-            <div className="h-[30px] w-[30px] center rounded-full  text-white font-semibold  ">
+          <div className="h-full justify-between items-end flex flex-col ">
+            <div className="h-[30px] w-[30px] center bg-[#ED2224] rounded-full  text-white font-semibold  ">
               1
             </div>
             <div className="flex text-[14px] text-nowrap w-full gap-4">
