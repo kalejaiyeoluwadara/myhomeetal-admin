9c46c96481e142c061774fcf0e48e4228b28c1fe
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-06-23 07:47:13 +0100

    updates

--- app/dashboard/components/Logout.jsx
+++ app/dashboard/components/Logout.jsx
@@ -1,9 +1,26 @@
+"use client";
+import { useGlobal } from "@/app/context";
 import React from "react";
 
 function Logout() {
+  const { logout, setLogOut } = useGlobal();
   return (
-    <div className="h-screen fixed top-0 z-40 w-full bg-black center bg-opacity-25 ">
-      Logout
+    <div className="h-screen fixed top-0 z-40 w-full bg-black flex justify-start items-center bg-opacity-25 ">
+      <div className="w-[400px] ml-[270px] h-[400px] p-[40px] bg-white rounded-2xl flex items-center justify-center flex-col ">
+        <div className="h-[68px]  w-[68px] bg-[#FFC5C6] rounded-full " />
+        <h2 className="text-[24px] leading-[29px] mt-4 font-bold text-center w-[277px]  ">
+          Are you sure you want to log out?
+        </h2>
+        <p className="text-center text-[#292929] mt-4 text-sm font-light w-[277px]  ">
+          Ensure you've saved all your actions before proceeding.
+        </p>
+        <button className="bg-[#] mt-6 mb-2 text-[16px] center w-full h-[44px] rounded-[8px] ">
+          Yes, Logout
+        </button>
+        <button className="bg-red-50 text-black center w-full h-[44px] rounded-[8px] ">
+          No, Cancel
+        </button>
+      </div>
     </div>
   );
 }
