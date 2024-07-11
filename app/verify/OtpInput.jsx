52f750e4fb5fa906b2a42027f3e857e3ce00f0f0
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-07-11 09:41:28 +0100

    updates

--- app/verify/OtpInput.jsx
+++ app/verify/OtpInput.jsx
@@ -14,27 +14,33 @@ const OtpVerification = ({ email }) => {
   };
 
   return (
-    <div className="border w-full h-400px rounded-24px flex items-start justify-center px-40px pr-24px flex-col border-DCDCDC gap-16px">
-      <h3 className="text-25px w-full text-center font-bold">
+    <div className="border my-[67px] w-full h-auto py-[44px] rounded-[24px] flex items-start justify-center px-[40px] pr-[24px] flex-col border-[#DCDCDC] gap-[16px]">
+      <h3 className="text-[25px] w-full text-center font-light">
         Enter Verification Code
       </h3>
-      <p className="w-275px">We have sent a verification code to {email}</p>
+      <div className="w-full center ">
+        <p className="w-[275px] text-[15px] text-center ">
+          We have sent a verification code to {email}
+        </p>
+      </div>
+      <p className="mt-[46px] mb-[10px] text-center w-full ">5 digit code</p>
       <OtpInput
         value={otp}
         onChange={handleChange}
         numInputs={5}
         separator={<span>-</span>}
+        renderInput={(props) => <input {...props} />}
         inputStyle="otp-input"
         containerStyle="otp-container"
       />
       <div className="w-full">
-        <div
-          className="cursor-pointer mt-4 w-full h-52px flex items-center justify-center rounded-99px text-16px font-bold bg-primary text-white"
+        <button
+          className="cursor-pointer mt-[84px] w-full h-[52px] flex items-center justify-center rounded-[99px] text-[16px] font-semibold "
           onClick={handleVerify}
         >
           Verify One time password
-        </div>
-        <p className="text-16px text-center mt-8px text-gray-600 font-medium">
+        </button>
+        <p className="text-[16px] text-center mt-[18px] text-gray-600 font-medium">
           Didn't receive the code?{" "}
           <a href="#">
             <span className="text-primary">Request a new code</span>
--- app/verify/page.jsx
+++ app/verify/page.jsx
@@ -1,17 +1,17 @@
 "use client";
 import Image from "next/image";
-import logo from "../app/assets/logo.svg";
-import man from "../app/assets/wall.svg";
-import OtpVerification from "../components/OtpInput";
+import logo from "@/app/assets/logo.svg";
+import man from "@/app/assets/wall.svg";
+import OtpVerification from "./OtpInput";
 
 const Page = () => {
   return (
-    <main className="min-h-screen w-full p-35px gap-60px flex items-start justify-center">
+    <main className="min-h-screen w-full p-[35px] gap-[60px] flex items-start justify-center">
       <section className="h-full w-40% flex flex-col items-start">
         <Image className="mb-81px" alt="Logo" src={logo} />
         <OtpVerification email="oyefesoafolabiteniola@gmail.com" />
       </section>
-      <section className="h-636px overflow-hidden w-50% relative rounded-24px">
+      <section className="h-[836px]  overflow-hidden w-[50%] relative rounded-[24px]">
         <Image className="cover" src={man} alt="Happy man" />
       </section>
     </main>
