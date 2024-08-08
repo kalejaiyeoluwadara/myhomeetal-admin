2030b3612b89035fc1ee6fa04353aedba4366670
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-08-08 11:29:10 +0100

    updated order table

--- app/dashboard/orders/comp/TableData.jsx
+++ app/dashboard/orders/comp/TableData.jsx
@@ -24,7 +24,7 @@ function TableData({
 
   return (
     <div className="w-full h-[72px] border-b bg-white text-[#344054] text-[14px] px-3 items-center justify-center grid pt-2 grid-cols-9 ">
-      <p className=" col-span-2 truncate ">{orderId}</p>
+      <p className=" col-span-2 truncate ">{"orderId"}</p>
       <div className="flex w-auto col-span-2 items-center justify-start gap-2 truncate">
         <p className="text-[14px] font-medium text-[#101928] ">{person}</p>
       </div>
@@ -33,7 +33,7 @@ function TableData({
       <div className="px-2 ">
         <p
           className={`px-3 font-medium ${
-            status === "Pending"
+            status !== "Delivered"
               ? "bg-[#FFF1F1] text-[#C70E10]  "
               : "text-green-600 bg-green-200"
           } rounded-[12px] py-[2px] flex items-center justify-center `}
