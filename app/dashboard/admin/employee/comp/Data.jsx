215c3a0b885fbdaaeba80bc79702358539d734c7
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-08-13 09:52:11 +0100

    fixed shrinking images

--- app/dashboard/admin/employee/comp/Data.jsx
+++ app/dashboard/admin/employee/comp/Data.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import sport from "@/app/assets/sport.svg";
-import Image from "next/image";
 import Link from "next/link";
 
 function Data({ productTitle, images, _id }) {
@@ -10,14 +8,16 @@ function Data({ productTitle, images, _id }) {
       : "https://images.unsplash.com/photo-1694878981819-1084b2d7dd0b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   return (
     <Link href={`/dashboard/products/${_id}`}>
-      <div className="flex justify-between gap-2 px-4 h-[68px] items-center">
-        <img
-          src={img}
-          className=" h-[40px] bg-gray-200 w-[40px] rounded-full object-cover "
-          alt=""
-        />
-        <h3>{productTitle}</h3>
-        <span className="pill  cursor-text ">Low</span>
+      <div className="flex justify-between items-center gap-2 px-4 h-[68px] w-full ">
+        <div className="flex gap-2 w-[200px] items-center ">
+          <img
+            src={img}
+            className=" h-[40px] flex-shrink-0  bg-gray-200 w-[40px] rounded-full object-cover "
+            alt=""
+          />
+          <h3 className="truncate">{productTitle}</h3>
+        </div>
+        <span className="pill px-2  cursor-text ">Low</span>
       </div>
     </Link>
   );
--- app/dashboard/products/comp/TableData.jsx
+++ app/dashboard/products/comp/TableData.jsx
@@ -31,7 +31,7 @@ function TableData({
     <div className="w-full h-[72px] border-b bg-white text-[#344054] text-[14px] px-3 items-center justify-center grid pt-2 grid-cols-8 ">
       <div className="flex w-auto col-span-2 items-center justify-start gap-2 truncate">
         <img
-          className=" h-[40px] bg-gray-200 w-[40px] rounded-full object-cover "
+          className=" h-[40px] flex-shrink-0 bg-gray-200 w-[40px] rounded-full object-cover "
           alt=""
           src={img}
         />
