6865589e363a8c8a637ab2adf6671b95e0a33e41
zumboxdev <zumboxdev@gmail.com>
2025-11-24 12:10:44 +0100

    Updated API URL

--- app/api/apiRoute.jsx
+++ app/api/apiRoute.jsx
@@ -1,4 +1,4 @@
 export class ApiRoutes {
-  static BASE_URL = "https://api.myhomeetal.store/api/v1/";
+  static BASE_URL = "https://api.myhomeetal.com/api/v1/";
   // static BASE_URL = "http://localhost:5000/api/v1/";
 }
